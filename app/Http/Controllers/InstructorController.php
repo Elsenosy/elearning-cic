90e1ee44c53c9a668cf33e3b963008c962c81edf
Taha Elsenosy <tahaelsenosy@gmail.com>
2021-09-05 14:17:51 +0200

    Inseting data with validation

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Instructor;
 
+
 class InstructorController extends Controller
 {
     public function index(){
@@ -43,7 +44,38 @@ class InstructorController extends Controller
     }
 
     // Store instructor data in database
-    public function store(){
-        dd("hh");
+    public function store(Request $request){
+        // Define rules
+        $rules = [
+            'name' => 'required|string|min:3|max:225',
+            'email' => 'required|email|max:225|unique:instructors,email',
+            'phone' => 'required|numeric|starts_with:0|unique:instructors,phone|digits_between:10,30',
+            'password' => 'required|string|min:6|max:10'
+        ];
+        
+        // Do validation
+        $this->validate($request, $rules);  
+        
+        // $instructor = new Instructor();
+        // $instructor->name = request()->name;
+        // $instructor->email = request()->email;
+        // $instructor->phone = request()->phone;
+        // $instructor->password = \Hash::make(request()->password);
+        // $instructor->save();
+
+        // Another way to save data
+        $data = request()->all();
+
+        // Check if request has password, to hash
+        if(isset($data['password']) || request()->has('password')){
+            $data['password'] = \Hash::make(request()->password);
+        }
+
+        Instructor::create($data);
+
+        // Return to back;
+        return back();
+
+        // dd(request()->all());
     }
 }
--- resources/views/instructors/create.blade.php
+++ resources/views/instructors/create.blade.php
@@ -11,37 +11,48 @@
         <div class="col-sm-12">
             <div class="card card-primary">
                 <div class="card-header">
-                    <h3 class="card-title">Instructor data</h3>
+                    <h3 class="card-title">Instructor data</h3> 
                 </div>
                 <!-- /.card-header -->
-                <!-- form start --  >
-                <form action="{{ url('instructors') }}">
+                <!-- form start -->
+                <form action="{{ url('instructors') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="card-body">
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                <h4>There is an error!</h4>
+                                <ul>
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                         <div class="form-group">
                             <label for="exampleInputEmail1">{{ __('lang.name') }}</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.name') }}" autocomplete="off">
+                            <input type="text" name="name" value="{{ old('name') }}" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.name') }}" autocomplete="off">
                         </div>
 
                         <div class="form-group">
                             <label for="exampleInputEmail1">{{ trans('lang.email') }}</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.email') }}" autocomplete="off">
+                            <input type="email" name="email" value="{{ old('email') }}" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.email') }}" autocomplete="off">
                         </div>
 
                         <div class="form-group">
                             <label for="exampleInputEmail1">{{ trans('lang.phone') }}</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.phone') }}" autocomplete="off">
+                            <input type="text" name="phone" value="{{ old('phone') }}" class="form-control" id="exampleInputEmail1" placeholder="{{ trans('lang.phone') }}" autocomplete="off">
                         </div>
 
                         <div class="form-group">
                             <label for="exampleInputPassword1">{{ __('lang.password') }}</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1"
+                            <input type="password" name="password" class="form-control" id="exampleInputPassword1"
                                 placeholder="{{ __('lang.password') }}">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputFile">File input</label>
                             <div class="input-group">
                                 <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="exampleInputFile">
+                                    <input type="file" name="avatar" class="custom-file-input" id="exampleInputFile">
                                     <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                                 </div>
                                 <div class="input-group-append">
